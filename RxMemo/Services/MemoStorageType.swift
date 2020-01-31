6246f666684611690fcc06eefb8ece6e9e08cdcf
DONGGUN LEE <d0n99un17@gmail.com>
2020-01-31 14:49:37 +0900

    Create MemoryStorage

---
+++ RxMemo/Services/MemoStorageType.swift
@@ -0,0 +1,24 @@
+//
+//  MemoStorageType.swift
+//  RxMemo
+//
+//  Created by DONGGUN LEE on 1/31/20.
+//  Copyright © 2020 Tong. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+protocol MemoStorageType {
+    @discardableResult
+    func createMemo(content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func memoList() -> Observable<[Memo]>
+    
+    @discardableResult
+    func update(memo: Memo, content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func delete(memo: Memo) -> Observable<Memo>
+}
