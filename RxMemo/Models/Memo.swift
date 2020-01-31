2754fddcfa05560f4124e2ac118687bbac59a65c
DONGGUN LEE <d0n99un17@gmail.com>
2020-01-31 12:02:50 +0900

    Create Memo Model

---
+++ RxMemo/Models/Memo.swift
@@ -0,0 +1,26 @@
+//
+//  Memo.swift
+//  RxMemo
+//
+//  Created by DONGGUN LEE on 1/31/20.
+//  Copyright © 2020 Tong. All rights reserved.
+//
+
+import Foundation
+
+struct Memo: Equatable {
+    var content: String
+    var insertDate: Date
+    var identity: String
+    
+    init(content: String, insertDate: Date = Date()) {
+        self.content = content
+        self.insertDate = insertDate
+        self.identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    init(original: Memo, updatedContent: String){
+        self = original
+        self.content = updatedContent
+    }
+}
