338fe0d1b1a6cadbdfc38ba196f11fd1104609dc
DONGGUN LEE <d0n99un17@gmail.com>
2020-01-31 15:02:42 +0900

    Create View Controllers & Connect Story Board

---
+++ RxMemo/ViewControllers/MemoDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MemoDetailViewController.swift
+//  RxMemo
+//
+//  Created by DONGGUN LEE on 1/31/20.
+//  Copyright © 2020 Tong. All rights reserved.
+//
+
+import UIKit
+
+class MemoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
