8d0957d02f60d89efc3f7b95d6cb1dbc4f2dcee8
DONGGUN LEE <d0n99un17@gmail.com>
2020-02-18 21:57:06 +0900

    Change Scene (List -> Detail)

--- RxMemo/Scenes/SceneCoordinator.swift
+++ RxMemo/Scenes/SceneCoordinator.swift
@@ -10,6 +10,12 @@ import Foundation
 import RxSwift
 import RxCocoa
 
+extension UIViewController{
+    var sceneViewController: UIViewController{
+        return self.children.first ?? self
+    }
+}
+
 class SceneCoordinator: ScenceCoordinatorType{
     
     private let bag = DisposeBag()
@@ -29,7 +35,7 @@ class SceneCoordinator: ScenceCoordinatorType{
         
         switch style {
         case .root:
-            currentVC = target
+            currentVC = target.sceneViewController
             window.rootViewController = target
             subject.onCompleted()
         case .push:
@@ -38,8 +44,12 @@ class SceneCoordinator: ScenceCoordinatorType{
                 break
             }
             
+            nav.rx.willShow.subscribe(onNext: {[unowned self] evt in
+                self.currentVC = evt.viewController.sceneViewController
+                }).disposed(by: bag)
+            
             nav.pushViewController(target, animated: animated)
-            currentVC = target
+            currentVC = target.sceneViewController
             subject.onCompleted()
             
         case .modal:
@@ -47,7 +57,7 @@ class SceneCoordinator: ScenceCoordinatorType{
                 subject.onCompleted()
             }
             
-            currentVC = target
+            currentVC = target.sceneViewController
         }
         
         return subject.ignoreElements()
@@ -58,7 +68,7 @@ class SceneCoordinator: ScenceCoordinatorType{
         return Completable.create{[unowned self] completable in
             if let presentingVC = self.currentVC.presentingViewController{
                 self.currentVC.dismiss(animated: animated){
-                    self.currentVC = presentingVC
+                    self.currentVC = presentingVC.sceneViewController
                     completable(.completed)
                 }
             }
--- RxMemo/ViewControllers/MemoDetailViewController.swift
+++ RxMemo/ViewControllers/MemoDetailViewController.swift
@@ -44,6 +44,12 @@ class MemoDetailViewController: UIViewController, ViewModelBindableType {
                     fatalError()
                 }
         }.disposed(by: rx.disposeBag)
+        
+//        var backButton = UIBarButtonItem(title: nil, style: .done, target: nil, action: nil)
+//        viewModel.title.drive(backButton.rx.title).disposed(by: rx.disposeBag)
+//        backButton.rx.action = viewModel.popAction
+//        navigationItem.hidesBackButton = true
+//        navigationItem.leftBarButtonItem = backButton
     }
 
 }
